db39254c084c8faab3611dafd2e79d0438c4802f
MJSindhu7 <jayasindhu.manda@sjsu.edu>
2020-09-20 23:24:41 -0700

    Compose tweet, retweet, favorite tweet implementation

--- Twitter/TweetCellTableViewCell.swift
+++ Twitter/TweetCellTableViewCell.swift
@@ -16,6 +16,90 @@ class TweetCellTableViewCell: UITableViewCell {
     
     @IBOutlet weak var tweetContent: UILabel!
     
+    @IBOutlet weak var retweetButton: UIButton!
+    
+    @IBOutlet weak var favButton: UIButton!
+    
+    
+    var favorited:Bool = false
+    var tweetId:Int = -1
+    //var retweeted:Bool = false
+    
+    @IBAction func favoriteTweet(_ sender: Any) {
+        let tobeFavorited = !favorited
+        if(tobeFavorited){
+            TwitterAPICaller.client?.favoriteTweet(tweetId: tweetId, success: {
+                self.setFavorite(true)
+            }, failure: { (Error) in
+                print("Favorite did not succeed: \(Error)")
+            })
+        } else{
+            TwitterAPICaller.client?.unfavoriteTweet(tweetId: tweetId, success: {
+                self.setFavorite(false)
+            }, failure: { (Error) in
+                 print("Unfavorite did not succeed: \(Error)")
+            })
+        }
+    }
+    
+    func setRetweeted(_ isRetweeted:Bool){
+        if(isRetweeted){
+            retweetButton.setImage(UIImage(named:"retweet-icon-green"), for: UIControl.State.normal)
+            retweetButton.isEnabled = false
+        }
+        else{
+            retweetButton.setImage(UIImage(named: "retweet-icon"), for: UIControl.State.normal)
+            retweetButton.isEnabled = true
+        }
+    }
+    
+    /*func favoriteTweet(tweetId: Int, success: @escaping () -> (), failure: @escaping (Error) -> ()){
+        let url = "https://api.twitter.com/1.1/favorites/create.json"
+        TwitterAPICaller.client?.post(url, parameters: ["id":tweetId], progress: nil, success: { (task: URLSessionDataTask, response: Any?) in
+            success()
+        }, failure: { (task: URLSessionDataTask?, error: Error) in
+            failure(error)
+        })
+    }
+    func unfavoriteTweet(tweetId: Int, success: @escaping () -> (), failure: @escaping (Error) -> ()){
+        let url = "https://api.twitter.com/1.1/favorites/destroy.json"
+        TwitterAPICaller.client?.post(url, parameters: ["id":tweetId], progress: nil, success: { (task: URLSessionDataTask, response: Any?) in
+            success()
+        }, failure: { (task: URLSessionDataTask?, error: Error) in
+            failure(error)
+        })
+    }
+    
+    func retweet(tweetId: Int, success: @escaping () -> (), failure: @escaping (Error) -> ()){
+           let url = "https://api.twitter.com/1.1/statuses/retweet/:id.json"
+           TwitterAPICaller.client?.post(url, parameters: ["id":tweetId], progress: nil, success: { (task: URLSessionDataTask, response: Any?) in
+               success()
+           }, failure: { (task: URLSessionDataTask?, error: Error) in
+               failure(error)
+           })
+       }*/
+    
+    @IBAction func retweet(_ sender: Any) {
+        TwitterAPICaller.client?.retweet(tweetId: tweetId, success: {
+            self.setRetweeted(true)
+        }, failure: { (Error) in
+            print("Error in retweeting:\(Error)")
+        })
+    }
+    
+
+    
+    
+    func setFavorite(_ isFavorited:Bool){
+        favorited = isFavorited
+        if(favorited){
+            favButton.setImage(UIImage(named:"favor-icon-red")  , for: UIControl.State.normal)
+        }
+        else{
+            favButton.setImage(UIImage(named:"favor-icon"), for: UIControl.State.normal)
+        }
+    }
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
